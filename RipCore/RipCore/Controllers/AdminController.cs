251bced1d461a251a8daccc5df103a6c3263e904
Magnús Gunnarsson <magnusgun15@ru.is>
2016-05-01 17:13:26 +0000

    view fyrir admin grunnur

--- RipCore/RipCore/Controllers/AdminController.cs
+++ RipCore/RipCore/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RipCore.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,25 @@ namespace RipCore.Controllers
 {
     public class AdminController : Controller
     {
+        private CourseService service = new CourseService();
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult User()
+        {
+            return View();
+        }
+        public ActionResult CourseOverview()
+        {
+            var viewModel = service.GetCoursesWhereStudent(2);
+            return View(viewModel);
+        }
+        public ActionResult PersonOverview()
+        {
+            return View();
+        }
+
     }
 }
