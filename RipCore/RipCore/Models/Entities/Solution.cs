e9fabe602d5182ff2527fc61773e186d17f4c7bf
Magnús Gunnarsson <magnusgun15@ru.is>
2016-05-02 20:46:03 +0000

    Eg er litill prins

---
+++ RipCore/RipCore/Models/Entities/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RipCore.Models.Entities
+{
+    public class Solution
+    {
+        public int ID { get; set; }
+        public int AssignmentID { get; set; }
+        public int StudentID { get; set; }
+
+
+    }
+}
---
+++ RipCore/RipCore/Models/Entities/Submission.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RipCore.Models.Entities
+{
+    public class Submission
+    {
+        public int ID { get; set; }
+        public int SolutionID { get; set; }
+        public string FileContent { get; set; }
+        public bool IsAccepted { get; set; }
+    }
+}
