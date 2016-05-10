3ef2a2977ea568784e5c694dd86d8339cc6a93dc
Magnús Gunnarsson <magnusgun15@ru.is>
2016-05-10 14:33:20 +0000

    Code cleaning og validation sans

--- RipCore/RipCore/Models/ViewModels/AdminCourseOverView.cs
+++ RipCore/RipCore/Models/ViewModels/AdminCourseOverView.cs
@@ -10,8 +10,12 @@ namespace RipCore.Models.ViewModels
     public class AdminCourseOverView
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [StringLength(4,ErrorMessage = "The year must have four digits",MinimumLength =4)]
         public int Year { get; set; }
+        [Required]
         public string Semester { get; set; }
         public Course toCourse() {
             Course courseToReturn = new Course {
